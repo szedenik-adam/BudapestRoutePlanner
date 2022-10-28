17bd6b75e927f51a64ee84271e9b352e5decbf62
Szedenik Adam <szedenik_adam@hotmail.com>
2022-10-28 22:03:29 +0200

    Improved timetable downloading (fetch API)

--- scripts/gtfsWorker.js
+++ scripts/gtfsWorker.js
@@ -6,89 +6,38 @@ postMessage('initializing');
 
 importScripts('progressLinearizer.js');
 importScripts('gtfs.js');
+importScripts('timetableLoader.js');
 try{importScripts('local.js');}catch(e){}
 importScripts('external/jszip.min.js');
 importScripts('external/idb.min.js');
 
-var gtfs, gtfsRoutes, db;
-
-async function getFeedAgeDays()
-{
-	let tx = db.transaction('cache');
-	let cacheStore = tx.objectStore('cache');
-	let caches = await cacheStore.getAll();
-	if (caches.length == 0) return 1000;
-	if (!('date' in caches[0])) return 1000;
-	return (new Date() - caches[0].date)/1000/3600/24;
-}
+var gtfsRoutes, db;
 
 async function initGTFS()
 {
-	var startTime = performance.now(), downloadEndTime=0;
 	
-	db = await idb.openDb('gtfs', 2, db => {
-		db.createObjectStore('cache', {keyPath: 'name'});
-		db.createObjectStore('tables', {keyPath: 'name'});
-		db.createObjectStore('extract', {keyPath: 'day'});
-	});
-	
-	//const feedAge = await getFeedAgeDays();
-	//console.log('feedAge', feedAge);
-	
-	var gtfs_zip = null; const dayNum = date2days((new Date()).toISOString().split('T')[0]);
-	var urls = [timetable_url+'common.json.zip', timetable_url+dayNum+'.json.zip']; 
-	var progresses = new Array(gtfs_urls.length).fill([0,100]);		
-	var blobsPromises = urls.map((url, ind) => {
-		return new Promise(function(resolve,reject){
-			const xhr = new XMLHttpRequest();
-			xhr.addEventListener('progress', function(e){
-				progresses[ind]=[e.loaded,e.total];
-				const downloaded = progresses.reduce((prevVal,curVal)=>prevVal+curVal[0],0);
-				const total = progresses.reduce((prevVal,curVal)=>prevVal+curVal[1],0);
-				postMessage({'progress':[`Timetable downloading ${humanReadableSize(downloaded)}/${humanReadableSize(total)}`, downloaded / total]});
-			});
-			xhr.onload = function(e) {
-			  if (this.status == 200) {
-				resolve(this.response);
-			  } else {
-				  console.log('rejecting json xhr', this);
-				  reject(null);
-			  }
-			};
-			xhr.open("GET", url);
-			xhr.responseType = 'blob';
-			xhr.send();
-			return xhr;
-		});
-	});
 	try {
-		const zipBlobs = await Promise.all(blobsPromises);
-		downloadEndTime = performance.now();
-		const zipContentPromises = zipBlobs.map(async blob => {
-			var zip = new JSZip();
-			zip = await zip.loadAsync(blob);
-			console.log('zip-parsed', zip, zip.files);
-			return Object.values(zip.files)[0].async("string");
+		db = await idb.openDb('gtfs', 3, db => {
+			for(const name of db.objectStoreNames) {
+				db.deleteObjectStore(name)
+			}
+			const cacheStore = db.createObjectStore('cache', {keyPath: 'name'});
+			const index = cacheStore.createIndex("timestamp", "timestamp");
+			db.createObjectStore('tables', {keyPath: 'name'});
+			db.createObjectStore('extract', {keyPath: 'day'});
 		});
-		const zipContents = await Promise.all(zipContentPromises);
-		const objects = zipContents.map(content => {
-			console.log('common content', content);
-			return JSON.parse(content);
-		});
-		console.log('zip timetables parsed');
-		gtfsRoutes = initRoutes(objects[0], objects[1], dayNum);
-	}catch(e){
-		console.log(e);
+	} catch(e) {
+		console.warn("Unable to open DB", e);
 	}
-
-	var initEndTime = performance.now();
-	const perfStr = `timetable init took ${(initEndTime - startTime)/1000} seconds (download: ${(downloadEndTime - startTime)/1000}, parse: ${(initEndTime - downloadEndTime)/1000})`
-	console.log(perfStr);
+	
+	var gtfs_zip = null; 
+	var timetableInfo = await LoadTimeTable();
+	gtfsRoutes = timetableInfo.routes;
 	
 	var result = route({lat:47.49990791402583,lon:19.080153604244394}, {lat:47.55545590531613,lon:19.043956781329452}, gtfsRoutes);
 	console.log('sending route to UI thread');
 	postMessage({'route':result});
-	postMessage({'info':perfStr+', routing: '+result.perf_sec});
+	postMessage({'info':timetableInfo.perf+', routing: '+result.perf_sec});
 }
 
 initGTFS();
---
+++ scripts/timetableLoader.js
@@ -0,0 +1,93 @@
+
+async function LoadTimeTable()
+{
+	var startTime = performance.now(), downloadEndTime=0, extractEndTime=0, parseEndTime=0;
+	const dayNum = date2days((new Date()).toISOString().split('T')[0]);
+	var urls = [timetable_url+'common.json.zip', timetable_url+dayNum+'.json.zip'];
+	var progresses = [];
+
+	var gtfsRoutes = null;
+	try {
+		const zipBlobs = await DownloadFiles(urls, progresses);
+		downloadEndTime = performance.now();
+		const zipContents = await ExtractZips(zipBlobs);
+		extractEndTime = performance.now();
+		const objects = ParseJsons(zipContents);
+		parseEndTime = performance.now();
+		console.log('zip timetables parsed');
+
+		if(objects[0].range[0] != objects[1].c_range[0] || objects[0].range[1] != objects[1].c_range[1])
+		{
+			console.log('reloading common');
+			const d2StartTime = performance.now();
+			const zipBlobs = await DownloadFiles([urls[0]], progresses, {cache: "reload"});
+			const d2EndTime = performance.now();
+			downloadEndTime += d2EndTime - d2StartTime;
+			const zipContents = await ExtractZips(zipBlobs);
+			const e2EndTime = performance.now();
+			extractEndTime += e2EndTime - d2EndTime;
+			const commonObjs = ParseJsons(zipContents);
+			const p2EndTime = performance.now();
+			parseEndTime += p2EndTime - e2EndTime;
+			objects[0] = commonObjs[0];
+			if(objects[0].range[0] != objects[1].c_range[0] || objects[0].range[1] != objects[1].c_range[1])
+			{
+				throw 'common and daily timetable mismatch!';
+			}
+		}
+		gtfsRoutes = initRoutes(objects[0], objects[1], dayNum);
+	}catch(e){
+		console.log(e);
+	}
+
+	var initEndTime = performance.now();
+	const perfStr = `timetable init took ${((initEndTime - startTime)/1000).toFixed(3)} seconds (download: ${((downloadEndTime - startTime)/1000).toFixed(3)}, extract: ${((extractEndTime - downloadEndTime)/1000).toFixed(3)}, parse: ${((parseEndTime - extractEndTime)/1000).toFixed(3)}, ctor: ${((initEndTime - parseEndTime)/1000).toFixed(3)})`
+	console.log(perfStr);
+	return {'routes':gtfsRoutes, 'perf':perfStr};
+}
+
+async function DownloadFiles(urls, progresses, fetchParams={})
+{
+	var blobsPromises = urls.map(async (url, ind) => {
+		progresses.unshift([0,0]);
+		var downloaded = 0;
+		var chunks = [];
+		var response = await fetch(urls[ind], fetchParams);  //, );
+		const reader = response.body.getReader();
+
+		const total = response.headers.get('Content-Length');
+
+		while(true) {
+		  const {done, value} = await reader.read();
+		  if (done) { break; }
+
+		  chunks.push(value);
+		  downloaded += value.length;
+		  
+		  progresses[ind]=[downloaded, total];
+		  const wholeDownloaded = progresses.reduce((prevVal,curVal)=>prevVal+curVal[0],0);
+		  const wholeTotal = progresses.reduce((prevVal,curVal)=>prevVal+curVal[1],0);
+		  postMessage({'progress':[`Timetable downloading ${humanReadableSize(wholeDownloaded)}/${humanReadableSize(total)}`, wholeDownloaded / total]});
+		}
+
+		return new Blob(chunks);
+	});
+	return Promise.all(blobsPromises);
+}
+async function ExtractZips(zipBlobs)
+{
+	const zipContentPromises = zipBlobs.map(async (blob,i) => {
+		var zip = new JSZip();
+		zip = await zip.loadAsync(blob);
+		console.log('zip-parsed', zip, zip.files);
+		return Object.values(zip.files)[0].async("string");
+	});
+	return Promise.all(zipContentPromises);
+}
+function ParseJsons(strings)
+{
+	const objects = strings.map(str => {
+		return JSON.parse(str);
+	});
+	return objects;
+}
